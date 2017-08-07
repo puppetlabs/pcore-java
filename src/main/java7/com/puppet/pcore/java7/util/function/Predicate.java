c6639576f687513bb5095f63fc13994e8122cce6
Thomas Hallgren <thomas@tada.se>
2017-08-07 16:18:51 +0200

    w.i.p.

--- src/main/java7/com/puppet/pcore/java7/util/function/Predicate.java
+++
@@ -1,5 +0,0 @@
-package com.puppet.pcore.java7.util.function;
-
-public interface Predicate<T> {
-    boolean test(T t);
-}
--- src/main/java7/com/puppet/pcore/java7/util/function/Supplier.java
+++
@@ -1,4 +0,0 @@
-package com.puppet.pcore.java7.util.function;
-public interface Supplier<T> {
-    T get();
-}
